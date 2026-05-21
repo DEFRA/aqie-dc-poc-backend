2c418b873f3493b4e921d3f220a1b336cf218571
F-mhow <fatima.mhowwala@Cognizant.com>
2026-05-21 13:50:22 +0100

    feat: added related tests

--- src/services/coords.js
+++ src/services/coords.js
@@ -1,12 +1,14 @@
-import OsGridRef from 'mt-osgridref'
+import OsGridRefImport from 'mt-osgridref'
 
 /**
  * convert OS BNG GAZETTEER_ENTRY.GEOMETRY_X/Y to WGS84 lat/lon
  * @param {{ x: number, y: number }} BNG
  * @returns {{ lat: nummber, lng: number }}
  */
+const OsGridRef = OsGridRefImport?.default ?? OsGridRefImport
+
 export function bngToLatLng({ x, y }) {
   const point = new OsGridRef(x, y)
-  const { _lat, _lng } = OsGridRef.osGridToLatLong(point)
-  return { lat: _lat, lng: _lng }
+  const { _lat, _lon } = OsGridRef.osGridToLatLong(point)
+  return { lat: _lat, lng: _lon }
 }
